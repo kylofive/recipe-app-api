a8f20193b7b388cbb64f7604b524164d60d96bf5
kylofive <kylofive@gmail.com>
2023-05-02 22:44:25 -0400

    fixed

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -12,7 +12,7 @@ from django.test import SimpleTestCase
 class CommandTest(SimpleTestCase):
 
     def test_wait_for_db_ready(self, patched_check):
-        "Test waiting for database if database ready"
+        """Test waiting for database if database ready"""
         patched_check.return_value = True
 
         call_command('wait_for_db')
@@ -21,7 +21,7 @@ class CommandTest(SimpleTestCase):
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
-        "test waiting for database when getting OperationalERROT"
+        """test waiting for database when getting OperationalERROT"""
 
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
