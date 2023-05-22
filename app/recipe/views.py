c2c8640dac9428b466418cc1c6edc3be6c30e4e9
kylofive <kylofive@gmail.com>
2023-05-22 17:27:34 -0400

    update

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,12 +2,18 @@
 View for the recipe API
 """
 
-from rest_framework import viewsets
+from rest_framework import (
+    viewsets,
+    mixins,
+)
 
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag,
+)
 from recipe import serializers
 
 
@@ -33,3 +39,20 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(
+        mixins.DestroModelMixin,
+        mixins.UpdateModelMixin,
+        mixins.ListModelMixin,
+        viewsets.GenericViewSet,):
+    """ manage tags in the database"""
+
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """ Filter querset to authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
